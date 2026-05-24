ae78bd102627b4aa7bad35daa8c76d828e8ab96a
Itburgas <fernando.pinero@itburgas.com>
2026-05-24 17:49:33 +0200

    Update index.js

--- api/contact/index.js
+++ api/contact/index.js
@@ -1,6 +1,11 @@
 module.exports = async function (context, req) {
     context.res = {
-        status: 200,
-        body: "API funcionando correctamente"
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            success: true,
+            message: "API OK"
+        }
     };
 };
